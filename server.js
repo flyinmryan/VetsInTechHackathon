00559810200b981c511a90c4c28124d3583da814
415DomSmith <415.dominic.smith@gmail.com>
2015-10-19 15:01:09 -0700

    added auth dependencies to server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var app = express();
 var morgan = require('morgan');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var passport = require('passport');
 
 require('./config/mongoose.js');
 require('./config/routes.js')(app);
@@ -23,6 +26,32 @@ app.use(morgan('tiny'));
 app.use(cookieParser()); // read cookies (needed for auth)
 // app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// Session-persisted message middleware
+// app.use(function(req, res, next){
+//  var err = req.session.error,
+//      msg = req.session.notice,
+//      success = req.session.success;
+
+//  delete req.session.error;
+//  delete req.session.success;
+//  delete req.session.notice;
+
+//  if (err) res.locals.error = err;
+//  if (msg) res.locals.notice = msg;
+//  if (success) res.locals.success = success;
+
+//  next();
+// });
+
+// Configure express to use handlebars templates
+// var hbs = exphbs.create({
+//    defaultLayout: 'main', //we will be creating this layout shortly
+// });
 
 app.set('view engine', 'ejs'); 
 
