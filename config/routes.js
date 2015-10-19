5ff3a0c1e5b86d145588fff87df2cd2c4217d6cf
Mike Ryan <flyinmryan@gmail.com>
2015-10-19 14:34:08 -0700

    routes

--- config/routes.js
+++ config/routes.js
@@ -8,31 +8,7 @@ module.exports = function(app){
 	// 	users.show(req, res);
 	// });
 
-	app.get('/home', function (req,res){  
-      res.format({        
-        'text/html': function(){
-          db.Issue.find({}).sort({votes: 'desc'}).limit(30).populate('user').exec(function (err, issues){
-          console.log('THIS IS FROM THE HTML/TEXT');
-            if (req.session.passport.user == null){
-              // console.log('NO ONE LOGGED IN');
-              res.render('home', {});
-            } else {
-              // console.log(req.session.passport.user + " IS LOGGED IN"); 
-              db.User.findById(req.session.passport.user, function (err, user){
-                res.render('home', {});
-              });
-            }
-          });
-        },
-        'application/json': function(){
-          var box = [req.query.NE, req.query.SW] //format req.query in to box of bounds    
-          // db.Issue.find({loc : {"$geoWithin" : {$box : box}}}).populate('user').sort({votes: 'desc'}).limit(30).exec(function (err, issues){ //find all issues inside box of bounds
-          //   res.send(issues);
-          })
-        },
-        'default': function(){
-          res.status(406).send('Error. Please Try Again');
-        }
-    });
+	app.get('/home', function (req,res){
+      
   });
 };
